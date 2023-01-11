2cda544a92f985794a971a935160fc5d222a5295
SamarChamkhi <101548708+SamarChamkhi@users.noreply.github.com>
2023-01-11 14:26:59 +0100

    Gérer les différents cas de Produit Matriciel

--- ProduitMatrice.go
+++ ProduitMatrice.go
@@ -17,27 +17,27 @@ func main() {
 		fmt.Print("Produit Matriciel Impossible Veuillez réessayer ")
 	}
 	fmt.Print("Rentrer les termes de la première Matrice = ")
-	for i = 0; i < lignes; i++ {
-		for j = 0; j < colonnes; j++ {
+	for i = 0; i < lignes1; i++ {
+		for j = 0; j < colonnes1; j++ {
 			fmt.Scan(&mat1[i][j])
 		}
 	}
 
 	fmt.Print("Rentrer les termes de la deuxième Matrice = ")
-	for i = 0; i < lignes; i++ {
-		for j = 0; j < colonnes; j++ {
+	for i = 0; i < lignes1; i++ {
+		for j = 0; j < colonnes1; j++ {
 			fmt.Scan(&mat2[i][j])
 		}
 	}
 
-	for i = 0; i < lignes; i++ {
-		for j = 0; j < colonnes; j++ {
+	for i = 0; i < lignes1; i++ {
+		for j = 0; j < colonnes2; j++ {
 			matProd[i][j] = mat1[i][j] * mat2[i][j]
 		}
 	}
 	fmt.Println("Le Résultat de Go pour la matrice produit = ")
-	for i = 0; i < lignes; i++ {
-		for j = 0; j < colonnes; j++ {
+	for i = 0; i < lignes1; i++ {
+		for j = 0; j < colonnes2; j++ {
 			fmt.Print(matProd[i][j], "\t")
 		}
 		fmt.Println()
