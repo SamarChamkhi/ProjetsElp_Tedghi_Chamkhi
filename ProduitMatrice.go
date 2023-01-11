f4083fa690ac4d9556fce85a74ad0ac32a8db4f2
SamarChamkhi <101548708+SamarChamkhi@users.noreply.github.com>
2023-01-11 14:24:32 +0100

    Gérer les différents cas de Produit Matriciel

--- ProduitMatrice.go
+++ ProduitMatrice.go
@@ -3,15 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	var lignes, colonnes, i, j int
+	var lignes1, colonnes1, lignes2, colonnes2, i, j int
 
 	var mat1 [10][10]int
 	var mat2 [10][10]int
 	var matProd [10][10]int
 
-	fmt.Print("Rentrer le nombre de Lignes puis celui de colonnes = ")
-	fmt.Scan(&lignes, &colonnes)
-
+	fmt.Print("Rentrer le nombre de Lignes puis celui de colonnes de la première Matrice = ")
+	fmt.Scan(&lignes1, &colonnes1)
+	fmt.Print("Rentrer le nombre de Lignes puis celui de colonnes de la deuxième Matrice = ")
+	fmt.Scan(&lignes2, &colonnes2)
+	if colonnes1 != lignes2 {
+		fmt.Print("Produit Matriciel Impossible Veuillez réessayer ")
+	}
 	fmt.Print("Rentrer les termes de la première Matrice = ")
 	for i = 0; i < lignes; i++ {
 		for j = 0; j < colonnes; j++ {
