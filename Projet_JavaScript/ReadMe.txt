2c60691b13fe0657dae817760004216f4d5bce55
Tedghi Noa <ntedghi@tc405-114-02.insa-lyon.fr>
2023-01-23 15:20:26 +0100

    readme

---
+++ Projet_JavaScript/ReadMe.txt
@@ -0,0 +1,9 @@
+Le programme permet de créer un shell en NodeJs. Voici les commandes pour l'utiliser :
+
+- 'lp' permet de lister les processus en cours et leur ID
+- 'bing [-k|-p|-c] <processId>' permet de tuer, mettre en pause ou reprendre un processus
+- 'CTRL-P' permet de sortir du shell
+- 'exec + programme ou chemin d'accès absolu ou relatif' permet d'exécuter au premier plan un programme du système d'exploitation
+- 'NomProgramme!' permet d'exécuter en tâche de fond un programme
+- 'keep <processId>' permet de détacher certains processus du CLIi  
+- 'path' indique l'endroi du path où le programme est executé
--- Projet_JavaScript/projetJs/projetJavascript.js
+++ Projet_JavaScript/projetJs/projetJavascript.js
@@ -1,8 +1,6 @@
-//import inquirer from "inquirer"
 const { exec, spawn } = require('child_process');
 const readline = require('readline');
 const path = require('path');
-//const inquirer = require("inquirer");
 // Récupération de la variable d'environnement PATH
 const envPath = process.env.PATH;
 
@@ -156,7 +154,7 @@ rl.on('line', (line) => {
               console.log(module.paths[0]);
               break;
           case 'keep':
-            processId=args[0]
+            let processId=args[0]
             // Vérifier que l'ID de processus est un nombre
             if (isNaN(processId)) {
               console.log(`Error: ${processId} is not a valid process ID.`);
@@ -164,104 +162,9 @@ rl.on('line', (line) => {
             }else{
               detache(processId);
             }
-              //let processId = args[0];
       }
     }
 })
 
-
-
-/*fct pour tache de fond : option 1
-Dans cet exemple, la fonction runBackgroundProcess() 
-//prend en argument une commande et des arguments, 
-//et l'exécute en utilisant spawn(). La fonction écoute 
-//les événements stdout, stderr et close pour obtenir 
-//les résultats de la commande et les afficher dans 
-//la console.
-
-const { spawn } = require('child_process');
-
-function runBackgroundProcess(command, args) {
-    const child = spawn(command, args);
-    child.stdout.on('data', (data) => {
-        console.log(`stdout: ${data}`);
-    });
-    child.stderr.on('data', (data) => {
-        console.error(`stderr: ${data}`);
-    });
-    child.on('close', (code) => {
-        console.log(`child process exited with code ${code}`);
-    });
-}*/
-// Boucle de lecture de ligne
-/*rl.on('line', (input) => {
-  // Récupération du chemin d'accès absolu ou relatif
-  const filePath = input.trim();
-  
-  // Ajout de la variable d'environnement PATH
-  process.env.PATH = `${filePath}:${envPath}`;
-  
-  // Exécution du programme
-  exec(filePath, (err, stdout, stderr) => {
-    if (err) {
-      console.error(`Error: ${err}`);
-      return;
-    }
-    console.log(`Output: ${stdout}`);
-  });
-});
-*/
-/*
-const { spawn } = require('child_process');
-const backgroundProcess = spawn('node', ['background.js'], { detached: true, stdio: 'ignore' });
-backgroundProcess.unref();*/
-
-/*Voici un exemple de code qui crée un shell en Node.js qui peut détacher des processus en 
-cours d'exécution en utilisant la commande keep <processId> :
-
-const readline = require('readline');
-const { exec } = require('child_process');
-
-// Création de l'interface de lecture de ligne
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-// Tableau pour stocker les processus à détacher
-const detachedProcesses = [];
-
-// Boucle de lecture de ligne
-rl.on('line', (input) => {
-  // Récupération de la commande et de l'ID de processus
-  const [command, processId] = input.trim().split(' ');
-
-  if (command === 'keep') {
-    // Vérifier que l'ID de processus est un nombre
-    if (isNaN(processId)) {
-      console.log(`Error: ${processId} is not a valid process ID.`);
-      return;
-    }
-
-    // Exécution de la commande détacher en utilisant l'option 'disown' pour détacher le processus
-    exec(`disown ${processId}`, (err) => {
-      if (err) {
-        console.error(`Error: ${err}`);
-        return;
-      }
-
-      // Ajout de l'ID de processus au tableau des processus détachés
-      detachedProcesses.push(processId);
-      console.log(`Process ${processId} has been detached.`);
-    });
-  } else {
-    console.log(`Unknown command: ${command}`);
-  }
-});
-Ce code utilise les modules readline pour créer une interface de lecture de ligne, child_process pour 
-exécuter une commande en utilisant exec(). Il utilise la méthode on pour écouter les entrées de l'utilisateur, 
-vérifie si la commande est "keep" et extrait l'ID de processus. Il vérifie si l'ID de processus est un nombre valide, 
-ensuite exécute la commande disown pour détacher le processus. Il ajoute également l'ID de processus détaché dans un 
-tableau pour une référence future.*/
 
 
