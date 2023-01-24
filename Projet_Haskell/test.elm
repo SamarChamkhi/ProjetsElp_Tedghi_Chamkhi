4dbce6e6b6a8180d49d37ff8280935a6e91f4c69
Tedghi Noa <ntedghi@2D-LINUX-002.insa-lyon.fr>
2023-01-24 11:17:19 +0100

    piste pour Haskell (a modifier et améliorer, ca ne marche pas encore

--- Projet_Haskell/test.elm
+++ Projet_Haskell/test.elm
@@ -1,67 +1,118 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (Html, Attribute, span, input, text)
-import Html.Attributes exposing (..)
-import Html.Events exposing (onInput)
-
-
-
--- MAIN
-
-
-main =
-  Browser.sandbox { init = init, update = update, view = view }
-
-
+import Html exposing (..)
+import Html.Attributes exposing (style)
+import Html.Events exposing (..)
+import Http
+import Json.Decode exposing (Decoder, map4, field, int, string)
+import File
+import Random
 
 -- MODEL
 
+type Model
+    = Failure
+    | Loading
+    | Success Word
 
-type alias Model =
-  { input : String
-  }
+type alias Word =
+    { word : String
+    , definition : String
+    , synonyms : String
+    }
 
-
-init : Model
-init =
-  { input = "" }
-
-
+init : () -> (Model, Cmd Msg)
+init _ =
+    (Loading, getRandomWordFromFile)
 
 -- UPDATE
 
-
 type Msg
-  = Change String
+    = MorePlease
+    | GotWord (Result Http.Error Word)
+    | GotFile (Result Http.Error String)
 
-
-update : Msg -> Model -> Model
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
-  case msg of
-    Change newInput ->
-      { model | input = newInput }
+    case msg of
+        MorePlease ->
+            (Loading, getRandomWordFromFile)
 
+        GotWord result ->
+            case result of
+                Ok word ->
+                    (Success word, Cmd.none)
 
+                Err _ ->
+                    (Failure, Cmd.none)
+
+        GotFile result ->
+            case result of
+                Ok contents ->
+                    let
+                        words = String.split " " contents
+                        word = Random.generate (Random.int 0 (List.length words - 1))
+                    in
+                    (Loading, Http.get { url = "https://api.dictionaryapi.dev/api/v2/entries/en/" ++ (List.head (List.drop word words)) , expect = Http.expectJson GotWord wordDecoder })
+                Err _ ->
+                    (Failure, Cmd.none)
+
+-- SUBSCRIPTIONS
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
 
 -- VIEW
 
-
 view : Model -> Html Msg
 view model =
-  case String.toFloat model.input of
-    Just celsius ->
-      viewConverter model.input "blue" (String.fromFloat (celsius * 1.8 + 32))
+    div []
+        [ h2 [] [ text "Random Word" ]
+        , viewWord model
+        ]
 
-    Nothing ->
-      viewConverter model.input "red" "???"
+viewWord : Model -> Html Msg
+viewWord model =
+    case model of
+        Failure ->
+            div []
+                [ text "I could not load a random word for some reason. "                , button [ onClick MorePlease ] [ text "Try Again!" ]
+                ]
 
+        Loading ->
+            text "Loading..."
 
-viewConverter : String -> String -> String -> Html Msg
-viewConverter userInput color equivalentTemp =
-  span []
-    [ input [ value userInput, onInput Change, style "width" "40px" ] []
-    , text "°C = "
-    , span [ style "color" color ] [ text equivalentTemp ]
-    , text "°F"
-    ]
+        Success word ->
+            div []
+                [ button [ onClick MorePlease, style "display" "block" ] [ text "More Please!" ]
+                , div [] [ text word.word ]
+                , p [ style "text-align" "right" ]
+                    [ text "— "                    , cite [] [ text word.synonyms ]
+                    , text ("mot:"++word++" definition : " ++ word.definition ++ " ")
+                    ]
+                ]
+
+-- FILE READING
+
+getRandomWordFromFile : Cmd Msg
+getRandomWordFromFile =
+    File.read "words.txt" GotFile
+
+-- HTTP
+
+wordDecoder : Decoder Word
+wordDecoder =
+    map4 Word
+        (field "word" string)
+        (field "synonyms" string)
+        (field "definition" string)
+
+main =
+    Browser.element
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
